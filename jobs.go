e41c6e15933077bf366daccff76d6cb595f395fc
Hideo Hattori <hattori-h@klab.com>
2014-01-10 17:35:33 +0900

    print jobs score

--- jobs.go
+++ jobs.go
@@ -3,20 +3,26 @@ package main
 import (
     "github.com/codegangsta/cli"
     "github.com/hhatto/nanairo"
+    "bytes"
     "fmt"
     "io"
     "encoding/json"
 )
 
+type healthReport struct {
+    Score int `json:"score"`
+}
+
 type Job struct {
     Name string `json:"name"`
     Url string `json:"url"`
     Color string `json:"color"`
+    HealthReport []healthReport `json:"healthReport"`
 }
 
 func getJobs(hostname string) ([]Job, error) {
     client := NewClient(hostname)
-    res, err := client.get("api/json")
+    res, err := client.get("api/json?depth=1")
     if err != nil {
         return nil, err
     }
@@ -34,17 +40,35 @@ func getJobs(hostname string) ([]Job, error) {
 }
 
 func jobs(c *cli.Context) {
-    if len(c.Args()) > 0 {
-        jobs, _ := getJobs(c.Args()[0])
-        for _, job := range jobs {
-            if job.Color == "blue" {
-                fmt.Println(nanairo.FgColor("#0c0", "✔") + " " + job.Name)
-            } else {
-                fmt.Println(nanairo.FgColor("#c00", "✔") + " " + job.Name)
-            }
-        }
-    } else {
+    if len(c.Args()) == 0 {
         fmt.Println("set target hostname")
+        return
+    }
+
+    jobs, _ := getJobs(c.Args()[0])
+    for _, job := range jobs {
+        // S
+        var j = bytes.NewBufferString("")
+        if job.Color == "blue" {
+            j.WriteString(nanairo.FgColor("#0c0", "✔"))
+        } else {
+            j.WriteString(nanairo.FgColor("#c00", "✔"))
+        }
+        j.WriteString("  ")
+
+        // W
+        if job.HealthReport[0].Score >= 80 {
+            j.WriteString(nanairo.FgColor("#f90", "☀"))
+        } else if job.HealthReport[0].Score >= 20 {
+            j.WriteString(nanairo.FgColor("#999", "☁"))
+        } else {
+            j.WriteString(nanairo.FgColor("#03c", "☂"))
+        }
+
+        // Name
+        j.WriteString("  " + job.Name)
+
+        fmt.Println(j.String())
     }
 }
 
